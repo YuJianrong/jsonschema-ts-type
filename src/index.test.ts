a07a0a62c630b81cd650e41fa76d8c6e32cd749f
Jianrong Yu <jianrong.yu@sap.com>
2021-05-26 14:22:55 -0700

    Object and const

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,9 +4,12 @@ import { Schema } from './index';
 type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y ? 1 : 2
   ? true
   : false;
+
 function Type<T extends true>(): T | undefined {
   return;
 }
+
+Type<Equal<undefined, undefined>>();
 
 const stringSchema = { type: 'string', minLength: 2 } as const;
 Type<Equal<Schema<typeof stringSchema>, string>>();
@@ -28,6 +31,9 @@ Type<Equal<Schema<typeof enumSchema>, null | 'null' | 1 | 'hello'>>();
 const stringEnumSchema = { type: 'string', enum: ['null', 'hello'] } as const;
 Type<Equal<Schema<typeof stringEnumSchema>, 'null' | 'hello'>>();
 
+const numberEnumSchema = { type: 'number', enum: [1, 2, 3] } as const;
+Type<Equal<Schema<typeof numberEnumSchema>, 1 | 2 | 3>>();
+
 const simpleUnionSchema = { type: ['boolean', 'string'] } as const;
 Type<Equal<Schema<typeof simpleUnionSchema>, string | boolean>>();
 
@@ -45,16 +51,210 @@ const typedTupleSchema = {
     { type: 'string', enum: ['Street', 'Avenue', 'Boulevard'] },
   ],
 } as const;
-Type<Equal<Schema<typeof typedTupleSchema>, [number, string, 'Street' | 'Avenue' | 'Boulevard']>>();
+Type<
+  Equal<
+    Schema<typeof typedTupleSchema>,
+    | []
+    | [number]
+    | [number, string]
+    | [number, string, 'Street' | 'Avenue' | 'Boulevard']
+    | [number, string, 'Street' | 'Avenue' | 'Boulevard', ...any[]]
+  >
+>();
 
-const untypedSchema = { type: 'array' } as const;
-Type<Equal<Schema<typeof untypedSchema>, any[]>>();
+const typedMinItemsTupleSchema = {
+  type: 'array',
+  items: [
+    { type: 'number' },
+    { type: 'string' },
+    { type: 'string', enum: ['Street', 'Avenue', 'Boulevard'] },
+  ],
+  minItems: 2,
+} as const;
+Type<
+  Equal<
+    Schema<typeof typedMinItemsTupleSchema>,
+    | [number, string]
+    | [number, string, 'Street' | 'Avenue' | 'Boulevard']
+    | [number, string, 'Street' | 'Avenue' | 'Boulevard', ...any[]]
+  >
+>();
 
-// type ZZ<T> = {[k in keyof T]: Schema<T[k]> };
-// type ZZZ = SimpleTupleSchema<[{"type": "number"},{"type": "string"}]>
+const typedNoAdditionalItemsTupleSchema = {
+  type: 'array',
+  items: [
+    { type: 'number' },
+    { type: 'string' },
+    { type: 'string', enum: ['Street', 'Avenue', 'Boulevard'] },
+  ],
+  additionalItems: false,
+} as const;
+Type<
+  Equal<
+    Schema<typeof typedNoAdditionalItemsTupleSchema>,
+    [] | [number] | [number, string] | [number, string, 'Street' | 'Avenue' | 'Boulevard']
+  >
+>();
 
-// type Z = Schema<[{ type: 'number' }, { type: 'string' }]>;
+const typedNoAdditionalMinItemsTupleSchema = {
+  type: 'array',
+  items: [
+    { type: 'number' },
+    { type: 'string' },
+    { type: 'string', enum: ['Street', 'Avenue', 'Boulevard'] },
+  ],
+  additionalItems: false,
+  minItems: 2,
+} as const;
+Type<
+  Equal<
+    Schema<typeof typedNoAdditionalMinItemsTupleSchema>,
+    [number, string] | [number, string, 'Street' | 'Avenue' | 'Boulevard']
+  >
+>();
 
-// type T = TupleSchema<DeepWriteable<typeof typedTupleSchema>>;
+const typedAdditionalStringTupleSchema = {
+  type: 'array',
+  items: [
+    { type: 'number' },
+    { type: 'string' },
+    { type: 'string', enum: ['Street', 'Avenue', 'Boulevard'] },
+  ],
+  additionalItems: { type: 'string' },
+} as const;
+Type<
+  Equal<
+    Schema<typeof typedAdditionalStringTupleSchema>,
+    | []
+    | [number]
+    | [number, string]
+    | [number, string, 'Street' | 'Avenue' | 'Boulevard']
+    | [number, string, 'Street' | 'Avenue' | 'Boulevard', ...string[]]
+  >
+>();
 
-// console.log('xx');
+const typedAdditionalStringMinItemsTupleSchema = {
+  type: 'array',
+  items: [
+    { type: 'number' },
+    { type: 'string' },
+    { type: 'string', enum: ['Street', 'Avenue', 'Boulevard'] },
+  ],
+  minItems: 2,
+  additionalItems: { enum: ['Good', null] },
+} as const;
+Type<
+  Equal<
+    Schema<typeof typedAdditionalStringMinItemsTupleSchema>,
+    | [number, string]
+    | [number, string, 'Street' | 'Avenue' | 'Boulevard']
+    | [number, string, 'Street' | 'Avenue' | 'Boulevard', ...('Good' | null)[]]
+  >
+>();
+
+const untypedArraySchema = { type: 'array' } as const;
+Type<Equal<Schema<typeof untypedArraySchema>, any[]>>();
+
+const objectSchema = {
+  type: 'object',
+  properties: {
+    number: { type: 'number' },
+    street_name: { type: 'string' },
+    street_type: { enum: ['Street', 'Avenue', 'Boulevard'] },
+  },
+} as const;
+Type<
+  Equal<
+    Schema<typeof objectSchema>,
+    {
+      number?: number;
+      street_name?: string;
+      street_type?: 'Street' | 'Avenue' | 'Boulevard';
+    } & Record<string, any>
+  >
+>();
+
+const objectNoAdditionalPropertiesSchema = {
+  type: 'object',
+  properties: {
+    number: { type: 'number' },
+    street_name: { type: 'string' },
+    street_type: { enum: ['Street', 'Avenue', 'Boulevard'] },
+  },
+  additionalProperties: false,
+} as const;
+Type<
+  Equal<
+    Schema<typeof objectNoAdditionalPropertiesSchema>,
+    {
+      number?: number;
+      street_name?: string;
+      street_type?: 'Street' | 'Avenue' | 'Boulevard';
+    }
+  >
+>();
+
+const objectSpecificAdditionalPropertiesSchema = {
+  type: 'object',
+  properties: {
+    number: { type: 'number' },
+    street_name: { type: 'string' },
+    street_type: { enum: ['Street', 'Avenue', 'Boulevard'] },
+  },
+  additionalProperties: { type: 'string' },
+} as const;
+Type<
+  Equal<
+    Schema<typeof objectSpecificAdditionalPropertiesSchema>,
+    {
+      number?: number;
+      street_name?: string;
+      street_type?: 'Street' | 'Avenue' | 'Boulevard';
+    } & Record<string, string>
+  >
+>();
+
+const objectPartialRequiredSchema = {
+  type: 'object',
+  properties: {
+    number: { type: 'number' },
+    street_name: { type: 'string' },
+    street_type: { enum: ['Street', 'Avenue', 'Boulevard'] },
+  },
+  additionalProperties: false,
+  required: ['number', 'street_name'],
+} as const;
+Type<
+  Equal<
+    Schema<typeof objectPartialRequiredSchema>,
+    {
+      number: number;
+      street_name: string;
+      street_type?: 'Street' | 'Avenue' | 'Boulevard';
+    }
+  >
+>();
+
+const constStringSchema = {
+  const: 'Hello',
+} as const;
+Type<Equal<Schema<typeof constStringSchema>, 'Hello'>>();
+
+const constNumberSchema = {
+  const: 1.23,
+} as const;
+Type<Equal<Schema<typeof constNumberSchema>, 1.23>>();
+
+// type Z = Schema<{
+//   description: 'Any validation failures are shown in the right-hand Messages pane.';
+//   type: 'object';
+//   properties: {
+//     foo: {
+//       type: 'number';
+//     };
+//     bar: {
+//       type: 'string';
+//       enum: ['a', 'b', 'c'];
+//     };
+//   };
+// }>;
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,102 @@
 type DeepWriteable<T> = { -readonly [P in keyof T]: DeepWriteable<T[P]> };
 
+// Primitive
 type StringSchema<T> = T extends { type: 'string' } ? string : never;
 type NumberSchema<T> = T extends { type: 'number' } ? number : never;
 type IntegerSchema<T> = T extends { type: 'integer' } ? number : never;
 type BooleanSchema<T> = T extends { type: 'boolean' } ? boolean : never;
 type NullSchema<T> = T extends { type: 'null' } ? null : never;
 
+// Enum
 type EnumSchema<T> = T extends { enum: any[] } ? T['enum'][number] : never;
-type EnumOrStringSchema<T> = EnumSchema<T> extends never ? StringSchema<T> : EnumSchema<T>;
 
+// Array
 type AnyArraySchema<T> = T extends { type: 'array' } ? any[] : never;
 type TypedArraySchema<T> = T extends { type: 'array'; items: Record<string, unknown> }
-  ? Schema<T['items']>[]
+  ? WritableSchema<T['items']>[]
   : never;
-type SimpleTupleSchema<T> = { [k in keyof T]: Schema<T[k]> };
-type TupleSchema<T> = T extends { type: 'array'; items: any[] }
-  ? SimpleTupleSchema<T['items']>
+
+// Tuple
+type LengthOf<T extends { length: number }> = T['length'];
+type Head<T> = T extends [...head: infer R, tail: any] ? R : never;
+type TupleUnion<A extends { length: number }, Min = 0> = LengthOf<A> extends Min
+  ? A
+  : TupleUnion<Head<A>, Min> | A;
+
+type SimpleTupleSchema<A extends any[]> = A extends [head: infer H, ...tail: infer T]
+  ? [WritableSchema<H>, ...SimpleTupleSchema<T>]
+  : [];
+
+type TupleMinItemsSchema<T> = T extends { type: 'array'; items: any[]; minItems: infer M }
+  ? TupleUnion<SimpleTupleSchema<T['items']>, M>
+  : T extends { type: 'array'; items: any[] }
+  ? TupleUnion<SimpleTupleSchema<T['items']>>
+  : never;
+
+type TupleAdditionalItemsSchema<T> = T extends {
+  type: 'array';
+  items: any[];
+  additionalItems: false;
+}
+  ? TupleMinItemsSchema<T>
+  : T extends {
+      type: 'array';
+      items: any[];
+      additionalItems: infer M;
+    }
+  ? TupleMinItemsSchema<T> | [...SimpleTupleSchema<T['items']>, ...WritableSchema<M>[]]
+  : T extends {
+      type: 'array';
+      items: any[];
+    }
+  ? TupleMinItemsSchema<T> | [...SimpleTupleSchema<T['items']>, ...any[]]
   : never;
 
 type TupleOrArraySchema<T> = T extends { type: 'array'; items: any[] }
-  ? TupleSchema<T>
+  ? TupleAdditionalItemsSchema<T>
   : T extends { type: 'array'; items: Record<string, unknown> }
   ? TypedArraySchema<T>
   : AnyArraySchema<T>;
 
+type OptionalObjectSchema<T> = T extends {
+  type: 'object';
+  properties: infer P;
+}
+  ? { [K in keyof P]?: Schema<P[K]> }
+  : never;
+
+// Object
+type ExpandObject<T> = { [K in keyof T]: T[K] };
+type PlainObjectSchema<T> = T extends {
+  type: 'object';
+  properties: infer P;
+  required: infer R;
+}
+  ? R extends string[]
+    ? ExpandObject<
+        { [K in Exclude<keyof P, R[number]>]?: Schema<P[K]> } &
+          { [K in Extract<keyof P, R[number]>]: Schema<P[K]> }
+      >
+    : OptionalObjectSchema<T>
+  : OptionalObjectSchema<T>;
+
+type ObjectSchema<T> = T extends {
+  type: 'object';
+  additionalProperties: false;
+}
+  ? PlainObjectSchema<T>
+  : T extends {
+      type: 'object';
+      additionalProperties: infer M;
+    }
+  ? PlainObjectSchema<T> & Record<string, WritableSchema<M>>
+  : T extends {
+      type: 'object';
+    }
+  ? PlainObjectSchema<T> & Record<string, any>
+  : never;
+
+// Simple Union (type: ['string', 'number'])
 type SimpleString<T> = T extends 'string' ? string : never;
 type SimpleNumber<T> = T extends 'number' ? number : never;
 type SimpleInteger<T> = T extends 'integer' ? number : never;
@@ -39,12 +112,24 @@ type SimpleUnionSchema<T> = T extends { type: ('string' | 'number' | 'boolean')[
   ? Simple<T['type'][number]>
   : never;
 
-type WriteableSchema<T> =
-  | EnumOrStringSchema<T>
+// Const
+type ConstSchema<T> = T extends { const: infer C } ? C : never;
+
+// Put together
+type NonEnumSchema<T> =
+  | StringSchema<T>
   | NumberSchema<T>
   | IntegerSchema<T>
   | BooleanSchema<T>
   | NullSchema<T>
   | SimpleUnionSchema<T>
-  | TupleOrArraySchema<T>;
-export type Schema<T> = WriteableSchema<DeepWriteable<T>>;
+  | TupleOrArraySchema<T>
+  | ObjectSchema<T>;
+
+type WritableSchema<T> = EnumSchema<T> extends never
+  ? ConstSchema<T> extends never
+    ? NonEnumSchema<T>
+    : ConstSchema<T>
+  : EnumSchema<T>;
+
+export type Schema<T> = WritableSchema<DeepWriteable<T>>;
